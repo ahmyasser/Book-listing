36aacecf7491bdd415ca71714f0ae817afe145f1
Ahmed Yasser <ahmedtricker@gmial.com>
2020-08-19 18:50:08 +0200

    done in es6 also

---
+++ appes6.js
@@ -0,0 +1,70 @@
+class Book {
+    constructor(title, auther, isbn) {
+        this.title = title;
+        this.auther = auther;
+        this.isbn = isbn;
+    }
+}
+class UI {
+    addBook = function(book) {
+        const list = document.getElementById('book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.auther}</td>
+            <td>${book.isbn}</td>
+            <td><a herf="#" class="delete">x<a/></td>
+            `
+        list.appendChild(row);
+    }
+    clearFields = function() {
+        document.getElementById('title').value = '';
+        document.getElementById('auther').value = '';
+        document.getElementById('isbn').value = '';
+    }
+
+    showAlert = function(msg, className) {
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(msg));
+        const container = document.querySelector('.container');
+        container.insertBefore(div, document.getElementById('book-form'));
+        setTimeout(function() {
+            container.removeChild(div);
+        }, 2000)
+    }
+
+    deleteBook = function(target) {
+        if (target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }
+    }
+}
+// Event lestiners
+document.getElementById('book-form').addEventListener('submit', function(e) {
+    // Declare variables
+    const title = document.getElementById('title').value,
+        auther = document.getElementById('auther').value,
+        isbn = document.getElementById('isbn').value,
+        book = new Book(title, auther, isbn),
+        ui = new UI()
+        // Validate input
+    if (title === '' || auther === '' || isbn === '') {
+        ui.showAlert('Please enter the data', 'error');
+    } else {
+        // Add book
+        ui.addBook(book);
+        ui.showAlert('Book Added!', 'success')
+            // Clear inpt fields
+        ui.clearFields();
+    }
+    e.preventDefault();
+})
+
+// Delete event listener
+document.getElementById('book-list').addEventListener('click', function(e) {
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    ui.showAlert('Deleted successfully!', 'success');
+    e.preventDefault();
+})
